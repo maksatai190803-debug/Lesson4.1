c1856ac2bef6c4c1bdab056f49a8c277946b4d7b
maksatai190803-debug <maksatai190803@gmail.com>
2025-11-26 23:27:50 +0600

    Home_works2

--- js/home_works.js
+++ js/home_works.js
@@ -1,7 +1,14 @@
 const gmailInput = document.getElementById("gmail_input");
 const gmailButtton = document.getElementById("gmail_button");
 const gmailResult = document.getElementById("gmail_result");
-const block = document.querySelector(".child_block");
+
+const childBlock = document.querySelector('.child_block');
+const parentBlock = document.querySelector('.parent_block');
+
+const secondsBlock = document.getElementById("seconds");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
 
 const regExp = /[\w.]+@[\w]+\.[a-zA-Z]{2,}/
 
@@ -15,14 +22,86 @@ gmailButtton.onclick = () => {
     }
 }
 
-let pos = 0; 
+let positionX = 0;
+let positionY = 0;
 
-function moveToRight() {
-    if (pos < 448) {        
-        pos++;
-        block.style.left = pos + "px";
-        requestAnimationFrame(moveToRight);
+const parentBlockWidth = parentBlock.clientWidth - childBlock.offsetWidth;
+const parentBlockHeight = parentBlock.clientHeight - childBlock.offsetHeight;
+
+let direction = "right"; 
+
+function moveBlock() {
+    childBlock.style.left = `${positionX}px`;
+    childBlock.style.top = `${positionY}px`;
+
+    if (direction === "right") {
+        positionX++;
+        if (positionX >= parentBlockWidth) direction = "down";
     }
+
+    else if (direction === "down") {
+        positionY++;
+        if (positionY >= parentBlockHeight) direction = "left";
+    }
+
+    else if (direction === "left") {
+        positionX--;
+        if (positionX <= 0) direction = "up";
+    }
+
+    else if (direction === "up") {
+        positionY--;
+        if (positionY <= 0) direction = "right";
+    }
+
+    requestAnimationFrame(moveBlock);
 }
 
-moveToRight();
+moveBlock();
+
+// function moveToRight() {
+//     if (pos < 448) {        
+//         pos++;
+//         block.style.left = pos + "px";
+//         requestAnimationFrame(moveToRight);
+//     }
+// }
+
+// moveToRight();
+
+let counter = 0;        
+let timerId = null;     
+let isRunning = false;
+
+function updateUI() {
+    secondsBlock.textContent = counter;
+}
+
+function startTimer() {
+    if (isRunning) return;          
+    isRunning = true;
+
+    timerId = setInterval(() => {
+        counter++;
+        updateUI();
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerId);
+    isRunning = false;             
+}
+
+function resetTimer() {
+    clearInterval(timerId);
+    counter = 0;
+    isRunning = false;
+    updateUI();                    
+}
+
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
+
+
+updateUI();
